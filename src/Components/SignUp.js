3a4079dd68e6e543ae56e65c09aff349a380e51d
Erfan Moghadasi <erfan.moqadasii@gmail.com>
2022-04-15 17:34:42 +0430

    add validation function and errors state

--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { validate } from "./validate";
 
 const SignUp = () => {
   const [data, setData] = useState({
@@ -9,35 +10,66 @@ const SignUp = () => {
     isAccepted: "",
   });
 
-  const changeHandler = event => {
-      if(event.target.name === 'isAccepted'){
-          setData({...data , [event.target.name] : event.target.checked})
-      }else{
-          setData({...data , [event.target.name] : event.target.value})
-      }
-  }
+  const [errors, setErrors] = useState({});
+
+  useEffect(() => {
+    setErrors(validate(data));
+  }, [data]);
+
+  const changeHandler = (event) => {
+    if (event.target.name === "isAccepted") {
+      setData({ ...data, [event.target.name]: event.target.checked });
+    } else {
+      setData({ ...data, [event.target.name]: event.target.value });
+    }
+  };
 
   return (
     <div>
       <div>
         <label>Name :</label>
-        <input type="text" name="name" value={data.name} onChange={changeHandler} />
+        <input
+          type="text"
+          name="name"
+          value={data.name}
+          onChange={changeHandler}
+        />
       </div>
       <div>
         <label>Email :</label>
-        <input type="text" name="email" value={data.email} onChange={changeHandler} />
+        <input
+          type="text"
+          name="email"
+          value={data.email}
+          onChange={changeHandler}
+        />
       </div>
       <div>
         <label>Password :</label>
-        <input type="password" name="password" value={data.password} onChange={changeHandler} />
+        <input
+          type="password"
+          name="password"
+          value={data.password}
+          onChange={changeHandler}
+        />
       </div>
       <div>
         <label>Config Password :</label>
-        <input type="password" name="confirmPassword" value={data.confirmPassword} onChange={changeHandler} />
+        <input
+          type="password"
+          name="confirmPassword"
+          value={data.confirmPassword}
+          onChange={changeHandler}
+        />
       </div>
       <div>
         <label>I accept terms of privacy policy :</label>
-        <input type="checkbox" name="isAccepted" value={data.isAccepted} onChange={changeHandler} />
+        <input
+          type="checkbox"
+          name="isAccepted"
+          value={data.isAccepted}
+          onChange={changeHandler}
+        />
       </div>
     </div>
   );
